b769f2c9f992a619bc1ec8629049a2c1b5f636f6
gabrielpiauilino-mlsa <145619516+gabrielpiauilino-mlsa@users.noreply.github.com>
2023-09-26 22:04:58 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Gabriel Piauilino",
+  title: "Data for All",
+  email: "Gabriel.PiauilinodeAlmeida@studentambassadors.com",
+  gitHub: "gabrielpiauilino-mlsa",
+  instagram: "",
+  linkedIn: "gabrielpiauilino",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
